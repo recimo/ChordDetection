0ddc2dd27b49cf43758455586277f511db07d568
recimo <nikola_ilic96@hotmail.com>
2019-07-10 03:06:57 +0200

    Prepoznavanje tonova i akorda
    Uradjeno prepoznavanje tonova na osnovu koordinata tacaka (vrhova prstiju)  u odnosu na zice i pragove. Zatim odlucivanje koji akord je u pitanju na osnovu prepoznatih tonova.

--- handDetection.py
+++ handDetection.py
@@ -3,28 +3,29 @@ import numpy as np
 import math
 from scipy.signal import argrelextrema
 
+#ucitavanje slike i resizovanje
 img = cv2.imread('a14.jpg')
-
 resized = cv2.resize(img, (512, 512), cv2.INTER_LINEAR)
 
+#hsv i treshold
 hsv_img = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
 lower_skin = np.array([0,55,95])
 upper_skin = np.array([10,175,255]) #namestaj H parametar kako bi izdvojio boju koze bolje
 
+#maska za boju koze
 skinMask = cv2.inRange(hsv_img, lower_skin, upper_skin)
-
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
 kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)) #kernel za eroziju
 skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
 skinMask = cv2.erode(skinMask, kernel, iterations = 2)
 skinMask = cv2.erode(skinMask, kernel2, iterations = 2) #dodatno erodirati za manje piksela (kernel2) zbog zica
-
 skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
 skinMask = cv2.medianBlur(skinMask, 19)
-skin = cv2.bitwise_and(hsv_img, hsv_img, mask=skinMask)
 
+skin = cv2.bitwise_and(hsv_img, hsv_img, mask=skinMask)
 image = cv2.cvtColor(skin, cv2.COLOR_HSV2BGR)
 
+#nalazenje kontura
 ret,thresh = cv2.threshold(skinMask, 0, 255, 0)
 im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
@@ -35,8 +36,10 @@ if len(contours) != 0:
     x,y,w,h = cv2.boundingRect(c)
     #cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
 
+#nalazenje tacaka na konturi
 hull = cv2.convexHull(c,returnPoints = False)
 defects = cv2.convexityDefects(c,hull)
+fingerTips = []
 
 for i in range(defects.shape[0]):
     s,e,f,d = defects[i,0]
@@ -45,10 +48,12 @@ for i in range(defects.shape[0]):
     far = tuple(c[f][0])
     #cv2.line(image,start,end,[0,255,0],2)
     cv2.circle(image,end,5,[0,0,255],-1)
+    fingerTips.append(end) #dodavanje koordinata vrhova prstiju u niz
 
-#cv2.imshow('image', np.hstack([resized, image]))
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+def prikaziKonturu():
+    cv2.imshow('image', np.hstack([resized, image]))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 ################ trazenje linija (zica) gitare ################
@@ -126,7 +131,7 @@ for line in lines:
 
 ###grupisi vertikalne###
 
-print(verticalLines)
+#print(verticalLines)
 
 verticalLinesTemp = verticalLines
 arrayX = []
@@ -164,7 +169,6 @@ fret4 = 0
 ima3 = False
 ima4 = False
 
-
 if meanX < 100:
     fret4 = meanX
     #da li je treci ili cetvrt
@@ -201,11 +205,95 @@ if ima4 == True:
     cv2.line(resized, (int(fret2), 0), (int(fret2), 512), (255, 0, 0), 3)
     cv2.line(resized, (int(fret1), 0), (int(fret1), 512), (255, 0, 0), 3)
 
+def prikaziZicePragove():
+    cv2.imshow("edges", edges)
+    cv2.imshow("image", resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imshow("edges", edges)
-cv2.imshow("image", resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
+strings.sort(reverse=True)
+tonovi = []
 
+def koordinateZicaPragova():
+    print('STRINGS: ', strings)
+    print(fret3, fret2, fret1)
 
+for ft in fingerTips:
+    if ft[0] > fret3 and ft[0] < fret2:
+        if ft[1] <= strings[5]:
+            tonovi.append("G")
+        elif ft[1] > strings[5] and ft[1] <= strings[4]:
+            tonovi.append("C")
+        elif ft[1] > strings[4] and ft[1] <= strings[3]:
+            tonovi.append("F")
+        elif ft[1] > strings[3] and ft[1] <= strings[2]:
+            tonovi.append("B")
+        elif ft[1] > strings[2] and ft[1] <= strings[1]:
+            tonovi.append("D")
+        elif ft[1] > strings[1] and ft[1] <= strings[0]:
+            tonovi.append("G")
+    elif ft[0] > fret2 and ft[0] < fret1:
+        if ft[1] <= strings[5]:
+            tonovi.append("F#")
+        elif ft[1] > strings[5] and ft[1] <= strings[4]:
+            tonovi.append("H")
+        elif ft[1] > strings[4] and ft[1] <= strings[3]:
+            tonovi.append("E")
+        elif ft[1] > strings[3] and ft[1] <= strings[2]:
+            tonovi.append("A")
+        elif ft[1] > strings[2] and ft[1] <= strings[1]:
+            tonovi.append("C#")
+        elif ft[1] > strings[1] and ft[1] <= strings[0]:
+            tonovi.append("F#")
+    elif ft[0] > fret1:
+        if ft[1] <= strings[5]:
+            tonovi.append("F")
+        elif ft[1] > strings[5] and ft[1] <= strings[4]:
+            tonovi.append("B")
+        elif ft[1] > strings[4] and ft[1] <= strings[3]:
+            tonovi.append("D#")
+        elif ft[1] > strings[3] and ft[1] <= strings[2]:
+            tonovi.append("G#")
+        elif ft[1] > strings[2] and ft[1] <= strings[1]:
+            tonovi.append("C")
+        elif ft[1] > strings[1] and ft[1] <= strings[0]:
+            tonovi.append("F")
+
+def ispisiTonove(tonovi):
+    tonovi = list(dict.fromkeys(tonovi)) #izbaci duplikate
+    print(tonovi)
+
+#koji akord je u pitanju na osnovu tonova
+akord = ""
+
+if "C" in tonovi and "E" in tonovi:
+    akord = "C dur"
+elif "A" in tonovi and "C#" in tonovi and "E" in tonovi:
+    akord = "A dur"
+elif "F" in tonovi and "C" in tonovi and "A" in tonovi:
+    akord = "F dur"
+elif "G" in tonovi and "H" in tonovi and "D" in tonovi:
+    akord = "G dur"
+elif "E" in tonovi and "G#" in tonovi and "H" in tonovi:
+    akord = "E dur"
+elif "D" in tonovi and "F#" in tonovi and "A" in tonovi:
+    akord = "D dur"
+elif "A" in tonovi and "C" in tonovi and "E" in tonovi:
+    akord = "A mol"
+elif "E" in tonovi and "H" in tonovi:
+    akord = "E mol"
+elif "D" in tonovi and "A" in tonovi and "F" in tonovi:
+    akord = "D mol"
+elif "B" in tonovi and "F" in tonovi:
+    akord = "B dur"
+elif "B" in tonovi and "F" in tonovi and "C#" in tonovi:
+    akord = "B mol"
+else
+    akord = "Akord nije prepoznat"
+
+#ispisiTonove(tonovi)
+#prikaziZicePragove()
+#koordinateZicaPragova()
+#prikaziKonturu()
+print(akord)
